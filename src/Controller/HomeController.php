04419807d420c726dfae1d5c9eb0b49c7f36f440
Zoé_Dazy <zoe.dazy+github@gmail.com~>
2023-09-29 16:17:05 +0200

    ajout home controller

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\UserRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route('/home', name: 'app_home')]
+    public function listUsers(UserRepository $userRepository): Response
+    {
+        $users = $userRepository->findAll();
+
+        return $this->render('home/index.html.twig', [
+            'users' => $users
+        ]);
+    }
+}
